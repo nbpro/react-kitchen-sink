15e33169cf12068871fe37ee99325f895b6ff99d
nbpro <nbneeraj8@gmail.com>
2021-01-17 23:33:56 +0530

    fix string to number comparison

--- src/calculator/components/CalculatorPad.tsx
+++ src/calculator/components/CalculatorPad.tsx
@@ -52,12 +52,24 @@ export default function CalculatorPad() {
             setDisplayValue(String(digit));
             setWaitingForOperand(false);
         } else {
-            setDisplayValue(displayValue === '0' ? String(digit) : displayValue + digit);
+            setDisplayValue(displayValue === '0' ? String(digit) : parseFloat(displayValue) + digit);
         }
     }
 
     function performOperation(key: any){
-        console.log(key);
+        const inputValue = parseFloat(displayValue);
+        if (value == null) {
+            // @ts-ignore
+            setValue(inputValue);
+        } else if (operator) {
+            const currentValue = value || 0
+            // @ts-ignore
+            const newValue = CalculatorOperations[key](currentValue, inputValue);
+            setValue(newValue);
+            setDisplayValue(String(newValue));
+        }
+        setWaitingForOperand(true);
+        setOperator(key);
     }
 
     function showResult():void{
@@ -65,29 +77,30 @@ export default function CalculatorPad() {
     }
     return (
         <CalculatorNumericPad>
+            Result is : {displayValue}
             <Row>
                 <Col span={6}> <Button type="text" value="1" onClick={() => handleClick(1)}>1</Button></Col>
                 <Col span={6}> <Button type="text" value="2" onClick={() => handleClick(2)}>2</Button></Col>
                 <Col span={6}> <Button type="text" value="3" onClick={() => handleClick(3)}>3</Button></Col>
-                <Col span={6}> <Button type="text" value="ADD" onClick={() => handleClick('ADD')}>Add(+)</Button></Col>
+                <Col span={6}> <Button type="text" value="ADD" onClick={() => handleClick('+')}>Add(+)</Button></Col>
             </Row>
             <Row>
                 <Col span={6}> <Button type="text" value="4" onClick={() => handleClick(4)}>4</Button></Col>
                 <Col span={6}> <Button type="text" value="5" onClick={() => handleClick(5)}>5</Button></Col>
                 <Col span={6}> <Button type="text" value="6" onClick={() => handleClick(6)}>6</Button></Col>
-                <Col span={6}> <Button type="text" value="SUB" onClick={() => handleClick('SUB')}>Subtract(-)</Button></Col>
+                <Col span={6}> <Button type="text" value="SUB" onClick={() => handleClick('-')}>Subtract(-)</Button></Col>
             </Row>
             <Row>
                 <Col span={6}> <Button type="text" value="7" onClick={() => handleClick(7)}>7</Button></Col>
                 <Col span={6}> <Button type="text" value="8" onClick={() => handleClick(8)}>8</Button></Col>
                 <Col span={6}> <Button type="text" value="9" onClick={() => handleClick(9)}>9</Button></Col>
-                <Col span={6}> <Button type="text" value="MUL" onClick={() => handleClick('MUL')}>Multiply(*)</Button></Col>
+                <Col span={6}> <Button type="text" value="MUL" onClick={() => handleClick('*')}>Multiply(*)</Button></Col>
             </Row>
             <Row>
                 <Col span={6}> <Button type="text" value="CLEAR" onClick={() => clearValue()}>Clear</Button></Col>
                 <Col span={6}> <Button type="text" value="0" onClick={() => handleClick(0)}>0</Button></Col>
-                <Col span={6}> <Button type="text" onClick={() => showResult()} value="RESULT">=</Button></Col>
-                <Col span={6}> <Button type="text" value="DIV" onClick={() => handleClick('DIV')}>Divide(/)</Button></Col>
+                <Col span={6}> <Button type="text" onClick={() => showResult()} value="=">=</Button></Col>
+                <Col span={6}> <Button type="text" value="DIV" onClick={() => handleClick('/')}>Divide(/)</Button></Col>
             </Row>
         </CalculatorNumericPad>
     );
